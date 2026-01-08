e426140ab9f3e1e54bdb55029cab8420f4239bc2
austintrose <austin.rose94@gmail.com>
2026-01-08 12:51:17 -0500

    r

--- src/data/about.ts
+++ src/data/about.ts
@@ -1,14 +1,6 @@
 export const aboutMarkdown = `# Intro
 
-Stuff
-I am the co-founder and CTO of [Promptfoo](https://promptfoo.dev), the most widely adopted open-source LLM security tool. We help teams discover and eliminate LLM risks through automated red-teaming and vulnerability analysis. Before Promptfoo, I was the VP of Engineering at [Smile ID](https://usesmileid.com), where I led teams building identity verification APIs that now process hundreds of millions of checks across Africa. I also co-founded [Arthena](https://arthena.com) (acquired by Masterworks) and [Matroid](https://matroid.com).
+[Download](https://austinrose.dev/resume.pdf)
 
-[Download](https://austinrose.dev/Austin_Rose_Resume.pdf)
-
-# I Like
-
-# Travel / Geography
-
-# Fun Facts
 
 `;
--- src/data/resume/degrees.ts
+++ src/data/resume/degrees.ts
@@ -6,18 +6,18 @@ export interface Degree {
 }
 
 const degrees: Degree[] = [
-  {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering (ICME)',
-    link: 'https://stanford.edu',
-    year: 2016,
-  },
-  {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
-  },
+  // {
+  //   school: 'Stanford University',
+  //   degree: 'M.S. Computational and Mathematical Engineering (ICME)',
+  //   link: 'https://stanford.edu',
+  //   year: 2016,
+  // },
+  // {
+  //   school: 'University at Buffalo',
+  //   degree: 'B.S. Electrical Engineering, Computer Engineering',
+  //   link: 'https://buffalo.edu',
+  //   year: 2012,
+  // },
 ];
 
 export default degrees;
